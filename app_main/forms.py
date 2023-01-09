8388f74f25d9d8680087f98707f42ff363c8c748
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-09 19:46:41 +0300

    Initial commit

---
+++ app_main/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from .models import *
+
+
+# class OrderCreateForm(forms.ModelForm):
+#     class Meta:
+#         model = Order
+#         fields = '__all__'
+#         widgets = {
+#             'user': forms.HiddenInput(),
+#             'paid': forms.HiddenInput(),
+#             'total_cost': forms.HiddenInput()
+#         }
