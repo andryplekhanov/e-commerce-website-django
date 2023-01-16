6ef0842c9e3a37d922f109913951ef59d20c1760
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-16 18:24:11 +0300

    настроил вывод продуктов на главную

--- app_main/views.py
+++ app_main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.db.models import Min
+from django.views.generic import TemplateView
+from app_product.models import Category, Product
 
 
-def index_view(request):
-    return render(request, 'app_main/index.html')
+class IndexView(TemplateView):
+    template_name = 'app_main/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # TODO категории задаются в настройках
+        categories = Category.objects.filter(parent__isnull=False)[:3]
+        cats = {}
+        for cat in categories:
+            cats[cat] = Product.objects.filter(category=cat).filter(available=True).only('price').aggregate(Min('price'))
+        context['cats'] = cats
+
+        limited = Product.objects.filter(available=True, limited=True).select_related('category').only('category', 'name', 'price')
+        context['limited'] = limited
+
+        return context
