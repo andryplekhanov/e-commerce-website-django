f4096c72b0b79d4ca814f3a950270523209970b4
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-22 11:39:58 +0300

    испр. проблемы с корзиной

--- app_cart/cart.py
+++ app_cart/cart.py
@@ -7,18 +7,19 @@ from django.shortcuts import render, redirect
 class Cart(object):
 
     def __init__(self, request):
+        """ Инициализируем корзину """
+
         self.request = request
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
-            # save an empty cart in the session
+            # сохраняем пустую корзину в сессии
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
     def add(self, product, quantity=1, action=None):
-        """
-        Add a product to the cart or update its quantity.
-        """
+        """ Добавляем товар в корзину или обновляем его количество. """
+
         id = product.id
         newItem = True
         if str(product.id) not in self.cart.keys():
@@ -31,17 +32,23 @@ class Cart(object):
                 'price': str(product.price),
                 'description': product.get_clear_description(),
                 'url': product.get_absolute_url(),
-                'image': str(product.images.first().image.url)
+                'image': str(product.images.first().image.url),
+                'stock': product.stock
             }
         else:
             newItem = True
 
             for key, value in self.cart.items():
                 if key == str(product.id):
-                    value['quantity'] = value['quantity'] + 1
-                    newItem = False
-                    self.save()
-                    break
+                    if value['quantity'] >= product.stock:
+                        newItem = False
+                        self.save()
+                        break
+                    else:
+                        value['quantity'] = value['quantity'] + 1
+                        newItem = False
+                        self.save()
+                        break
             if newItem == True:
                 self.cart[product.id] = {
                     'userid': self.request,
@@ -51,26 +58,29 @@ class Cart(object):
                     'price': str(product.price),
                     'description': product.get_clear_description(),
                     'url': product.get_absolute_url(),
-                    'image': str(product.images.first().image.url)
+                    'image': str(product.images.first().image.url),
+                    'stock': product.stock
                 }
         self.save()
 
     def save(self):
-        # update the session cart
+        """ Обновление корзины в сессии """
+
         self.session[settings.CART_SESSION_ID] = self.cart
-        # mark the session as "modified" to make sure it is saved
+        # помечаем сессию как изменённую, чтобы убедиться в сохранении
         self.session.modified = True
 
     def remove(self, product):
-        """
-        Remove a product from the cart.
-        """
+        """ Удаление продукта из корзины """
+
         product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
 
     def decrement(self, product):
+        """ Уменьшение количества товара на 1 ед. """
+
         for key, value in self.cart.items():
             if key == str(product.id):
 
@@ -83,6 +93,7 @@ class Cart(object):
                 print("Something Wrong")
 
     def clear(self):
-        # empty cart
+        """ Пустая корзина """
+
         self.session[settings.CART_SESSION_ID] = {}
         self.session.modified = True
