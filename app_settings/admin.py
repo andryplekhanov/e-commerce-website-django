c85d5e0f191110ae5955678ec5c7e7053eaaa1fe
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-22 18:06:14 +0300

    доб. модель Настройки

--- app_settings/admin.py
+++ app_settings/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.db.utils import ProgrammingError
+from app_settings.models import SiteSettings
 
-# Register your models here.
+
+class SiteSettingsAdmin(admin.ModelAdmin):
+    """ В админ-панели нужно создать экземпляр с настройками """
+
+    def has_add_permission(self, request, obj=None):
+        """ Запрещает создать более 1го экземпляра с настройками """
+        if not self.model.objects.all():
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        """ Запрещает удалять экземпляр с настройками """
+        return False
+
+
+admin.site.register(SiteSettings, SiteSettingsAdmin)
