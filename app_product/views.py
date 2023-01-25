7f9d406e02de0f5a67df8ae1bb03f5301e600a44
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-25 16:47:22 +0300

    изменение поведения при доб.тов. в корзину

--- app_product/views.py
+++ app_product/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import DetailView
 
+from app_cart.cart import Cart
 from app_product.models import Product
 from app_review.forms import ReviewForm
 from app_review.models import Review
@@ -16,6 +17,13 @@ class ProductDetail(DetailView):
         reviews = Review.objects.filter(product=self.get_object()).prefetch_related('user').order_by('-created')
         context['review_form'] = ReviewForm()
         context['reviews'] = reviews
+
+        context['is_product_in_cart'] = False
+        cart = Cart(self.request)
+        if len(cart) > 0:
+            all_ids = cart.get_all_ids()
+            if self.kwargs.get('pk') in all_ids:
+                context['is_product_in_cart'] = True
         return context
 
     def post(self, request, pk):
