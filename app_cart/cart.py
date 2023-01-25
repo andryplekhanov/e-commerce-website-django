e9a25e89628b1fcfca52ee63fbf3c1e91310af6e
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-25 16:44:12 +0300

    доб.фун. Подсчет всех товаров в корзине и Получить список id продуктов

--- app_cart/cart.py
+++ app_cart/cart.py
@@ -7,7 +7,6 @@ from app_product.models import Product
 
 
 class Cart(object):
-
     def __init__(self, request):
         """ Инициализируем корзину """
 
@@ -18,6 +17,10 @@ class Cart(object):
             # сохраняем пустую корзину в сессии
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def __len__(self):
+        """ Подсчет всех товаров в корзине. """
+        return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, action=None):
         """ Добавляем товар в корзину или обновляем его количество. """
@@ -109,6 +112,11 @@ class Cart(object):
 
         total_price = sum(float(item['price']) * item['quantity'] for item in self.cart.values())
         return Decimal.from_float(total_price).quantize(Decimal("1.00"))
+
+    def get_all_ids(self):
+        """ Получить список id продуктов в корзине  """
+
+        return [item.get('product_id') for item in self.cart.values()]
 
     def __iter__(self):
         """ Перебор элементов в корзине и получение продуктов из базы данных. """
