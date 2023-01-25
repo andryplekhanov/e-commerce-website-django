37a4fcbef61562ad2ffa157e3a309769e68e70d1
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-25 18:31:44 +0300

    популярные товары на главной

--- app_main/views.py
+++ app_main/views.py
@@ -1,6 +1,9 @@
 from django.db.models import Min
 from django.views.generic import TemplateView
 from app_product.models import Category, Product
+from django.db.models import Sum
+
+from app_settings.models import SiteSettings
 
 
 class IndexView(TemplateView):
@@ -9,9 +12,17 @@ class IndexView(TemplateView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        limited = Product.objects\
+        context['limited'] = Product.objects\
             .filter(available=True, limited=True)\
             .select_related('category')\
             .only('category', 'name', 'price')
-        context['limited'] = limited
+
+        settings = SiteSettings.load()
+        context['populars'] = Product.objects\
+            .prefetch_related('order_items')\
+            .filter(available=True)\
+            .only('category', 'name', 'price')\
+            .annotate(total=Sum('order_items__quantity'))\
+            .order_by('-total')[:settings.quantity_popular]
+
         return context
