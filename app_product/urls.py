bc045e38aec3e465b0f71f73ef350a854c3ac74c
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-26 17:37:57 +0300

    доб. сортировку товаров в каталоге

--- app_product/urls.py
+++ app_product/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('<int:pk>/', ProductDetail.as_view(), name='product_detail'),
+    path('catalog/', CategoryView.as_view(), name='catalog'),
+    path('catalog/<str:slug>/', CategoryView.as_view(), name='catalog_for_category'),
 ]
--- app_product/views.py
+++ app_product/views.py
@@ -2,11 +2,15 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import DetailView
+from django_filters.views import FilterView
 
 from app_cart.cart import Cart
-from app_product.models import Product
+from app_product.filters import ProductFilter
+from app_product.models import Product, Category
+from app_product.utils import get_data_min, get_data_max
 from app_review.forms import ReviewForm
 from app_review.models import Review
+from app_settings.models import SiteSettings
 
 
 class ProductDetail(DetailView):
@@ -36,3 +40,44 @@ class ProductDetail(DetailView):
             review.product = self.get_object()
             review.save()
         return HttpResponseRedirect(reverse('product_detail', kwargs={"pk": pk}))
+
+
+class CategoryView(FilterView):
+    paginate_by = 8
+    model = Product
+    context_object_name = 'products'
+    template_name = 'app_product/catalog.html'
+    filterset_class = ProductFilter
+
+    def get(self, request, *args, **kwargs):
+        self.settings = SiteSettings.load()
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        if not slug:
+            self.category = self.settings.root_category
+        else:
+            self.category = Category.objects.get(slug=slug)
+        sub_categories = self.category.get_descendants(include_self=True)
+        queryset = Product.objects\
+            .select_related('category')\
+            .only('name', 'price', 'category')\
+            .filter(category__in=sub_categories)\
+            .filter(available=True)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        _request_copy = self.request.GET.copy()
+        parameters = _request_copy.pop('page', True) and _request_copy.urlencode()
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.category
+        qs = self.get_queryset()
+        min_price = get_data_min(queryset=qs, root_category=self.settings.root_category)
+        max_price = get_data_max(queryset=qs, root_category=self.settings.root_category)
+        context['parameters'] = parameters
+        context['filter'].form.fields['price'].widget.attrs = {'class': 'range-line',
+                                                               'data-type': 'double',
+                                                               'data-min': min_price,
+                                                               'data-max': max_price}
+        return context
