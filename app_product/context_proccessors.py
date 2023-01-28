e4d477ac4ebfeb969fea6d479f6a51ce1e6dfd3c
Andrey Plekhanov <1989plekhanov@gmail.com>
2023-01-28 13:44:43 +0300

    отобр. категорий в футере

--- app_product/context_proccessors.py
+++ app_product/context_proccessors.py
@@ -6,6 +6,8 @@ def load_menu(request):
     try:
         settings = SiteSettings.load()
         sub_categories = settings.root_category.get_descendants(include_self=False)
+        base_categories = Category.objects.filter(parent_id=settings.root_category.id).only('name', 'slug')
     except AttributeError as exc:
         sub_categories = {}
-    return {'main_menu': sub_categories}
+        base_categories = {}
+    return {'main_menu': sub_categories, 'base_menu': base_categories}
